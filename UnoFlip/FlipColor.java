875452469ca320ce4d2da9b14a0c7e997207b83e
Saahib M. Singh <saahibmaansingh@gmail.com>
2026-02-15 18:51:52 -0500

    FlipColor.java

---
+++ UnoFlip/FlipColor.java
@@ -0,0 +1,15 @@
+public enum FlipColor {
+    // Light side colors
+    LIGHT_PINK,
+    LIGHT_TEAL,
+    LIGHT_PURPLE,
+    LIGHT_ORANGE,
+    LIGHT_WILD,
+
+    // Dark side colors
+    DARK_PINK,
+    DARK_TEAL,
+    DARK_PURPLE,
+    DARK_ORANGE,
+    DARK_WILD
+}

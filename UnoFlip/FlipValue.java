9f36d975954a1ece37a6906b5728fc1565ce7595
Saahib M. Singh <saahibmaansingh@gmail.com>
2026-02-15 18:52:15 -0500

    Remove commented section from FlipValue enum

---
+++ UnoFlip/FlipValue.java
@@ -0,0 +1,20 @@
+public enum FlipValue {
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+
+    // Light side special cards
+    LIGHT_SKIP,
+    LIGHT_REVERSE,
+    LIGHT_DRAW_ONE,
+    LIGHT_FLIP,
+    LIGHT_WILD,
+    LIGHT_WILD_DRAW_TWO,
+
+    // Dark side special cards
+    DARK_SKIP,
+    DARK_REVERSE,
+    DARK_DRAW_FIVE,
+    DARK_SKIP_EVERYONE,
+    DARK_FLIP,
+    DARK_WILD,
+    DARK_WILD_DRAW_COLOR
+}
